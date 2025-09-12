fcd5905b32118eafa205796c637aa4ec2fea24e0
foxnathan1190 <foxnathan1190@gmail.com>
2025-09-12 14:41:24 -0500

    Completed Solutions for album, movie, and their controllers. Added the movie controller.

--- src/main/java/com/example/mycollections/controllers/MovieController.java
+++ src/main/java/com/example/mycollections/controllers/MovieController.java
@@ -1,4 +1,45 @@
 package com.example.mycollections.controllers;
 
+import com.example.mycollections.models.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+    private final List<Movie> movies = new ArrayList<>() {{
+        add(new Movie("Ghostbusters", 1984, "Ivan Reitman", 105));
+        add(new Movie("The Emperor's New Groove", 2000, "Mark Dindal", 78));
+        add(new Movie("Coming to America", 1988, "John Landis", 116));
+    }};
+
+    @GetMapping("/json")
+    public List<Movie> getMoviesJson() {
+        return movies;
+    }
+
+    @GetMapping("/html")
+    public String getMoviesHtml() {
+        String movieList = "<ul>";
+        for (Movie movie : movies) {
+            movieList += "<li>" + movie + "</li>";
+        }
+        movieList += "</ul>";
+
+        return """
+                <html>
+                    <body>
+                        <h1>Movies</h1>
+                        <ul>
+                """ +
+                movieList +
+                """
+                        </ul>
+                    </body>
+                """;
+    }
 }
